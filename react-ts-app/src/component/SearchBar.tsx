b78485699320a71d1b5383e0b81d058118651d6f
alex96 <alex.96_2012@mail.ru>
2023-03-17 06:53:24 +0900

    feat: add componentWillUnmount in localStorage

--- react-ts-app/src/component/SearchBar.tsx
+++ react-ts-app/src/component/SearchBar.tsx
@@ -1,11 +1,40 @@
 import React from "react";
 import classes from "./SearchBar.module.css";
-class SearchBar extends React.Component {
+type Props = {
+  value?: string;
+};
+
+type State = {
+  value?: string;
+};
+
+class SearchBar extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      value: localStorage.getItem("value") || "",
+    };
+    this.changeInput = this.changeInput.bind(this);
+  }
+
+  componentWillUnmount() {
+    localStorage.setItem("value", `${this.state.value}`);
+  }
+  changeInput(e: React.ChangeEvent<HTMLInputElement>): void {
+    this.setState({
+      value: e.target.value,
+    });
+  }
   render() {
     return (
       <div className={classes.searchContainer}>
         <div>
-          <input type="text" placeholder="Search" />
+          <input
+            type="text"
+            placeholder="Search"
+            value={this.state.value}
+            onChange={this.changeInput}
+          />
           <button className={classes.button} type="submit">
             ss
           </button>
